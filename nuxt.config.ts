d6ab1cd96b1fcdfa50a86141b0ace74e834c68d6
Louis <louismcs@hotmail.co.uk>
2023-02-24 14:22:14 +0000

    Initial styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
+    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css', '~/assets/styles/main.scss'],
     build: {
         transpile: ['vuetify']
     }
